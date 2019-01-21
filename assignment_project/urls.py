939128f335af8dc2c0f1f460c043c34e550da489
Güzide Nur Demir <guzidedemir@std.sehir.edu.tr>
2019-01-21 12:37:35 +0300

    Html files were fixed

--- assignment_project/urls.py
+++ assignment_project/urls.py
@@ -1,10 +1,12 @@
 
-from django.conf.urls import  url, include
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^Lab', include('Lab.urls'))
+    url(r'^Lab', include('Lab.urls')),
+
+    url(r'Tool', include('Tool.urls'))
 
 ]
